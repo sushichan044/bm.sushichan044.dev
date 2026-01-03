34aa45f0e63ffb9c62eeef66d09430b28e6bbc4a
sushichan044 <71284054+sushichan044@users.noreply.github.com>
2026-01-03 21:35:21 +0900

    do not declare variables on top level

--- src/hondoko.ts
+++ src/hondoko.ts
@@ -1,30 +1,35 @@
 type ISBNExtractor = (url: URL, html: string) => string | undefined;
 
-const booklogExtractor: ISBNExtractor = (_, html) => {
-  // ISBN・EAN: 9784798640310
-  const match = /ISBN・EAN:\s*([0-9]{13})/.exec(html);
-  if (match) {
-    return match[1];
-  }
-  return undefined;
-};
+function extractISBN(url: URL, html: string): string | undefined {
+  const booklogPattern = new URLPattern({
+    hostname: "booklog.jp",
+    pathname: "/item/1/:pageId",
+    protocol: "http{s}?",
+  });
 
-const booklogPattern = new URLPattern({
-  hostname: "booklog.jp",
-  pathname: "/item/1/:pageId",
-  protocol: "http{s}?",
-});
+  const booklogExtractor: ISBNExtractor = (_, html) => {
+    // ISBN・EAN: 9784798640310
+    const match = /ISBN・EAN:\s*([0-9]{13})/.exec(html);
+    if (match) {
+      return match[1];
+    }
+    return undefined;
+  };
+
+  let isbn: string | undefined;
+  switch (true) {
+    case booklogPattern.test(url):
+      isbn = booklogExtractor(url, html);
+      break;
+  }
+
+  return isbn;
+}
 
 function main() {
   const currentUrl = new URL(document.URL);
   const html = document.documentElement.innerHTML;
-
-  let isbn: string | undefined;
-  switch (true) {
-    case booklogPattern.test(currentUrl):
-      isbn = booklogExtractor(currentUrl, html);
-      break;
-  }
+  const isbn = extractISBN(currentUrl, html);
 
   if (isbn === undefined) {
     window.alert("Could not extract ISBN.");
